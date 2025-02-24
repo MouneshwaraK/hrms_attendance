97206f168947596f2119a474531107bff4c7aef7
Mouneshwara <mouneshwara@clientservertech.com>
2025-02-24 15:10:27 +0530

    Updated code

--- lib/loginUI/login_ui.dart
+++ lib/loginUI/login_ui.dart
@@ -267,7 +267,7 @@ class _LoginUIState extends State<LoginUI> {
         "image": prefixedBase64Image,
       };
       Response response = await ApiService().validateUser(
-        url: "http://34.235.114.150:8000/face_recognition/",
+        url: "http://34.229.118.216:8000/face_recognition/",
         reqObj: jsonEncode(payload),
         device_status: "in",
       );
